bcacd515935a424fc90e2e94111b506d431ff0bf
Vlad Coteanu <vladcoteanu1997@gmail.com>
2019-04-01 23:55:43 +0300

    Finished movie detail component

--- src/main/java/org/fmi/movietracker/service/custom/impl/SimilarityCustomServiceImpl.java
+++ src/main/java/org/fmi/movietracker/service/custom/impl/SimilarityCustomServiceImpl.java
@@ -7,9 +7,7 @@ import org.fmi.movietracker.service.custom.SimilarityCustomService;
 import org.fmi.movietracker.service.dto.MovieDTO;
 import org.fmi.movietracker.service.mapper.MovieMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,7 +17,7 @@ import java.util.stream.Collectors;
 @Service
 public class SimilarityCustomServiceImpl implements SimilarityCustomService {
 
-    public static int NUMBER_OF_SUGESTIONS = 10;
+    public static int NUMBER_OF_SUGGESTIONS = 12;
 
     @Autowired
     private SimilarityRepository similarityRepository;
@@ -31,14 +29,14 @@ public class SimilarityCustomServiceImpl implements SimilarityCustomService {
     @Transactional
     public List<MovieDTO> getSuggestionsForMovie(Movie movie) {
         return similarityRepository.getSuggestionsForMovie(movie).stream()
-        .limit(NUMBER_OF_SUGESTIONS).map(p -> movieMapper.toDto(p)).collect(Collectors.toList());
+        .limit(NUMBER_OF_SUGGESTIONS).map(p -> movieMapper.toDto(p)).collect(Collectors.toList());
     }
 
     @Override
     @Transactional
     public List<MovieDTO> getSuggestionsForMovieAndUser(Movie movie, User user) {
         return similarityRepository.getSuggestionsForMovieAndUser(movie, user).stream()
-            .limit(NUMBER_OF_SUGESTIONS).map(p -> movieMapper.toDto(p)).collect(Collectors.toList());
+            .limit(NUMBER_OF_SUGGESTIONS).map(p -> movieMapper.toDto(p)).collect(Collectors.toList());
     }
 
 }
